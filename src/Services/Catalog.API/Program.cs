ad665be0fcb534dd9866aeef114c0039b77ba387
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-11 11:51:36 +0300

    Configure modules for carter

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,6 +1,14 @@
+using Catalog.API.Products.CreateProduct;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(null, config => config.WithModule<CreateProductEndpoint>());
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapCarter();
 
 app.Run();
