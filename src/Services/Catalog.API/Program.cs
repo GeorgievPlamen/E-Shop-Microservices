c7dc85a4acb0dcca4c7e8afc717b173dc1bdca26
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-12 07:09:32 +0300

    Setting up infra

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter(null, config => config.WithModule<CreateProductEndpoint>());
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblyContaining<Program>());
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
