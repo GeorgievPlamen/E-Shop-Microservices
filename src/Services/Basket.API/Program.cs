45c30986640c205bdf0de2dfb12c3baa04399a42
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-14 08:41:55 +0300

    Get basket

--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,5 +1,12 @@
+using Basket.API.Basket.GetBasket;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter(null, config => config.WithModules(
+    typeof(GetBasketEndpoints)));
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
