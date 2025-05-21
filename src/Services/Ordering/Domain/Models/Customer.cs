91aedd632a2fa7293db2dc2d0e9e4e9b4dbf4d65
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-21 08:51:12 +0300

    Create customer

--- src/Services/Ordering/Domain/Models/Customer.cs
+++ src/Services/Ordering/Domain/Models/Customer.cs
@@ -1,9 +1,25 @@
 using Domain.Abstractions;
+using Domain.ValueObjects;
 
 namespace Domain.Models;
 
-public class Customer : Entity<Guid>
+public class Customer : Entity<CustomerId>
 {
     public string Name { get; private set; } = null!;
     public string Email { get; private set; } = null!;
+
+    public static Customer Create(CustomerId id, string name, string email)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+        var customer = new Customer
+        {
+            Id = id,
+            Name = name,
+            Email = email
+        };
+
+        return customer;
+    }
 }
