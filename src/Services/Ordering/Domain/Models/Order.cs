4e9a7041bcc8df29ff2dda95e899db722d8d3861
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-23 20:36:10 +0300

    Added order methods

--- src/Services/Ordering/Domain/Models/Order.cs
+++ src/Services/Ordering/Domain/Models/Order.cs
@@ -15,4 +15,62 @@ public class Order : Aggregate<OrderId>
     public Payment Payment { get; private set; } = null!;
     public OrderStatus Status { get; private set; } = OrderStatus.Pending;
     public decimal TotalPrice => OrderItems.Sum(x => x.Price * x.Quantity);
+
+    public static Order Create(
+        OrderId id,
+        CustomerId customerId,
+        OrderName orderName,
+        Address shippingAddress,
+        Address billingAddress,
+        Payment payment,
+        OrderStatus status)
+    {
+        var order = new Order()
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = status
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(
+        OrderName orderName,
+        Address shippingAddress,
+        Address billingAddress,
+        Payment payment,
+        OrderStatus status)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = status;
+
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+
+        if (orderItem is not null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+    }
 }
--- src/Services/Ordering/Domain/Models/Product.cs
+++ src/Services/Ordering/Domain/Models/Product.cs
@@ -1,10 +1,18 @@
+using System.Net.Sockets;
 using Domain.Abstractions;
 using Domain.ValueObjects;
 
 namespace Domain.Models;
 
-public class Prodct : Entity<ProductId>
+public class Product : Entity<ProductId>
 {
     public string Name { get; private set; } = null!;
+
     public decimal Price { get; private set; }
+    public static Product Create(ProductId id, string name, decimal price) => new()
+    {
+        Id = id,
+        Name = name,
+        Price = price
+    };
 }
