5fd9ec9e017c8146dd44c18b69001ceaf384dbbc
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-27 08:18:51 +0300

    Auditing interceptor

--- src/Services/Ordering/Infrastructure/DI.cs
+++ src/Services/Ordering/Infrastructure/DI.cs
@@ -1,3 +1,6 @@
+using Infrastructure.Data;
+using Infrastructure.Data.Interceptors;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +11,14 @@ public static class DI
     public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration config)
     {
         var connectionString = config.GetConnectionString("Database");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.UseSqlServer(connectionString);
+        });
+
+        // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
     }
