ed8879b5714aabbb8104b18f16cab2f0df7a5425
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-27 08:31:10 +0300

    Add mediator

--- src/Services/Ordering/Application/DI.cs
+++ src/Services/Ordering/Application/DI.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -6,6 +7,10 @@ public static class DI
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
 
         return services;
     }
